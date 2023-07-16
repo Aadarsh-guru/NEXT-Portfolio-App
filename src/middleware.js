6b6e4c1e867c69570f358c8ae37bf6df083acf6f
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-16 15:44:39 +0530

    middleware changes

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,7 @@ export function middleware(NextRequest) {
 
 export const config = {
     matcher: [
-        '/dashboard/:path*',
+        '/dashboard',
         '/login',
         '/register'
     ]
