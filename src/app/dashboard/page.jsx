0ba4944b39b3f2362013f3e0c1212c38a568b70b
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-16 20:48:44 +0530

    change dash

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,19 +1,7 @@
-'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import './page.css'
-import { useData } from '@/context/DataProvider'
-import { useRouter } from 'next/navigation'
 
 const Dashboard = () => {
-
-    const { user } = useData()
-    const router = useRouter()
-
-    useEffect(() => {
-        if (!user?.email) {
-            router.push('/login')
-        }
-    }, [])
 
     return (
         <div>Dashboard</div>
