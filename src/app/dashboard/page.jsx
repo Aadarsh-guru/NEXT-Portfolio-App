fa64a8cda6b6733c7f30b2f9bbdb5c19af6c3c87
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-16 20:40:54 +0530

    protected dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import './page.css'
+import { useData } from '@/context/DataProvider'
+import { useRouter } from 'next/navigation'
 
 const Dashboard = () => {
+
+    const { user } = useData()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!user?.email) {
+            router.push('/login')
+        }
+    }, [user])
+
     return (
         <div>Dashboard</div>
     )
