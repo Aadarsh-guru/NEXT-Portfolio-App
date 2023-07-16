5c3eb537ccc2d1bd259a9c8d1f53817b92b867a6
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-16 15:48:19 +0530

    changes in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(NextRequest) {
-    const path = NextRequest.nextUrl.pathname
-    const token = NextRequest.cookies.get('token')?.value
+    const path = NextRequest?.nextUrl?.pathname
+    const token = NextRequest?.cookies?.get('token')?.value
     if (path === '/dashboard' && !token) {
-        return NextResponse.redirect(new URL('/login', NextRequest.url))
+        return NextResponse.redirect(new URL('/login', NextRequest?.url))
     } else if ((path === '/login' || path === '/register') && token) {
-        return NextResponse.redirect(new URL('/', NextRequest.url))
+        return NextResponse.redirect(new URL('/', NextRequest?.url))
     }
 }
 
