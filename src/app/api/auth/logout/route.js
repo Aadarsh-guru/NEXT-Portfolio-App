9b139b41c55abb70b146049fe34a1d05b5c521ea
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-18 16:29:16 +0530

    logout cokkie

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -4,9 +4,7 @@ export const GET = async () => {
     try {
         const response = NextResponse.json({ message: 'logout successfully.', success: true }, { status: 200 })
         response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) })
-        if (!response.cookies.get('token')?.value) {
-            return response;
-        }
+        return response;
     } catch (error) {
         return NextResponse.json({ message: error.message, success: false }, { status: 500 })
     }
