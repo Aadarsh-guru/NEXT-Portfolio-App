e1d407af1f0ccd5f7a0bd317e2c0dea95b56c7b0
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-18 16:15:46 +0530

    some chngaes

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,117 @@
+'use client'
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+        '& > .MuiDrawer-paper': {
+            marginLeft: '74px',
+            marginTop: 100,
+            height: 'calc(100vh - 150px)',
+            background: '#000',
+            color: '#fff',
+            borderTop: '1px solid gray',
+            borderRight: '1px solid gray'
+        }
+    }),
+);
+
+export default function Sidebar() {
+
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    return (
+        <Drawer variant="permanent" open={open}>
+            <DrawerHeader>
+                <IconButton onClick={() => setOpen(!open)}>
+                    {
+                        open ?
+                            <ChevronLeftIcon style={{ color: 'white' }} />
+                            :
+                            <MenuIcon style={{ marginRight: 4, color: 'white' }} />
+                    }
+                </IconButton>
+            </DrawerHeader>
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+                        <ListItemButton
+                            sx={{
+                                minHeight: 48,
+                                justifyContent: open ? 'initial' : 'center',
+                                px: 2.5,
+                            }}
+                        >
+                            <ListItemIcon
+                                sx={{
+                                    minWidth: 0,
+                                    mr: open ? 3 : 'auto',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <InboxIcon style={{ color: 'white' }} />
+                            </ListItemIcon>
+                            <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}
--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(NextRequest) {
     const path = NextRequest?.nextUrl?.pathname
     const token = NextRequest?.cookies?.get('token')?.value
-    if (path === '/dashboard' && !token) {
+    if ((path === '/dashboard') && !token) {
         return NextResponse.redirect(new URL('/login', NextRequest?.url))
     } else if ((path === '/login' || path === '/register') && token) {
         return NextResponse.redirect(new URL('/', NextRequest?.url))
@@ -12,7 +12,7 @@ export function middleware(NextRequest) {
 
 export const config = {
     matcher: [
-        '/dashboard',
+        '/dashboard/:path*',
         '/login',
         '/register'
     ]
