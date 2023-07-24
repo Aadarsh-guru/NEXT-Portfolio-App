c56f4a9d3daa7c6c6a89432056e07c07a91767ff
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-07-24 20:29:25 +0530

    middleware chnages

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export function middleware(NextRequest) {
     const path = NextRequest?.nextUrl?.pathname
     const token = NextRequest?.cookies?.get('token')?.value
-    if ((path === '/dashboard') && !token) {
+    if ((path === '/dashboard' || path?.includes('/dashboard/')) && !token) {
         return NextResponse.redirect(new URL('/login', NextRequest?.url))
     } else if ((path === '/login' || path === '/register') && token) {
         return NextResponse.redirect(new URL('/', NextRequest?.url))
