f9d59bdcf6c9fe04073ce0c8fc72b5841615994d
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-08-05 19:56:50 +0530

    portfolio updated

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './page.css'
+import Hero from "public/hero.png";
+import styles from "./page.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Portfolio = () => {
     return (
-        <div className="">
-
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <h1 className={styles.title}>
+                    Cheers to seamless web solutions that make your business hop!
+                </h1>
+                <p className={styles.desc}>
+                    Unleashing the power of web technology for your success.
+                </p>
+                <Link href={'/portfolio'}>
+                    <button className={styles.btn}>See Our Portfolio</button>
+                </Link>
+            </div>
+            <div className={styles.item}>
+                <Image src={Hero} alt="" className={styles.img} />
+            </div>
         </div>
     )
 }
