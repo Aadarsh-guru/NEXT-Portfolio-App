4cc614ba8deb8c291bd0dc81ab09ca01d46645e5
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-08-08 10:55:39 +0530

    moved the project on top added in category page

--- src/app/api/project/[category]/route.js
+++ src/app/api/project/[category]/route.js
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export const GET = async (NextRequest, { params }) => {
     await connection();
     try {
-        const data = await Project.find({ category: params?.category }).sort({ createdAt: 'descending' })
+        const data = await Project.find({ category: params?.category }).sort({ updatedAt: 'descending' })
         return NextResponse.json({ message: 'Projects fetched successfully.', success: true, projects: data }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ message: error.message, success: false }, { status: 500 })
